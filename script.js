e613c236a090d68ff4db980eefe3680a37416541
Jan Švanda <john.svanda@gmail.com>
2019-10-30 18:21:43 +0100

    js done

--- script.js
+++ script.js
@@ -1,108 +1,8 @@
-let container = document.getElementById("container");
-const mq = window.matchMedia("(min-width: 850px)");
-var filterBool = true;
-var aboutBool = false;
-var personBool = false;
-
-
-
-addClass("about");
-addClass("person");
-
 function addClass(classs){
     let classElements = document.getElementById(classs).querySelectorAll("*")
-    console.log(classElements);
     for (var i = 0; i < classElements.length; i++) {
         classElements[i].classList.add(classs);
     }
-}
-
-
-
-document.addEventListener("click", function (e) {
-    console.log(e.target);
-
-    if (filterBool == true && (e.target.id == "arrow" || e.target.id == "show")) {
-        closeFilter();
-    }
-    else if (filterBool == false && (e.target.id && "arrow" || e.target.id == "show")) {
-        openFilter();
-    }
-    else if (aboutBool == true && (e.target.className != "about")) {
-        closeAbout();
-    }
-
-    else if (aboutBool == false && e.target.id == "aboutButton") {
-        openAbout();
-    }
-    else if (personBool == true && (e.target.className != "person")) {
-        closePerson();
-        console.log(e);
-    }
-
-    else if (personBool == false && e.target.className == "person-photo") {
-        openPerson();
-    }
-
-
-})
-
-function openAbout() {
-    aboutBool = true;
-    document.getElementById("about").style.padding = "3em";
-    document.getElementById("all").style.display = "block";
-    document.getElementById("closebtn").style.display = "block";
-    disableScroll();
-
-    if (mq.matches) {
-        document.getElementById("about").style.width = "70vw";
-    }
-    else if (!mq.matches) {
-        document.getElementById("about").style.width = "90vw";
-    }
-
-}
-
-function closeAbout() {
-    aboutBool = false;
-    document.getElementById("about").style.width = "0";
-    document.getElementById("about").style.padding = "0";
-    document.getElementById("all").style.display = "none";
-    document.getElementsByTagName("body")[0].style.overflowX = "hidden";
-    allowScroll();
-}
-
-function openPerson() {
-    personBool = true;
-
-    document.getElementById("person").style.padding = "2em";
-    document.getElementById("all").style.display = "block";
-    document.getElementById("right-button").style.display = "block";
-    document.getElementById("left-button").style.display = "block";
-    disableScroll();
-
-    document.getElementById("button-void").style.display = "block";
-
-    if (mq.matches) {
-        document.getElementById("person").style.width = "55vw";
-    }
-    else if (!mq.matches) {
-        document.getElementById("person").style.width = "90vw";
-    }
-
-    document.getElementById("button-void").style.height = document.getElementById("person").offsetHeight + "px";
-
-}
-
-function closePerson() {
-    personBool = false;
-    document.getElementById("person").style.width = "0";
-    document.getElementById("person").style.padding = "0";
-    document.getElementById("all").style.display = "none";
-    document.getElementById("right-button").style.display = "none";
-    document.getElementById("left-button").style.display = "none";
-    document.getElementById("button-void").style.display = "none";
-    allowScroll()
 }
 
 function disableScroll() {
@@ -115,17 +15,132 @@ function allowScroll() {
     container.style.overflow = "scroll";
 }
 
-function closeFilter() {
-    filterBool = false;
-    document.getElementById("filter-content").style.display = "none";
-    document.getElementById("show").textContent = "SHOW FILTER";
-    document.getElementById("arrow").style.transform = 'rotate(180deg)';
+var container = document.getElementById("container");
+var all = document.getElementById("all");
+const mq = window.matchMedia("(min-width: 850px)");
+
+//About thickbox
+var about = document.getElementById("about");
+var closeButton = document.getElementById("closebtn");
+
+//Detail thickbox
+var person = document.getElementById("person");
+var rightButton = document.getElementById("right-button");
+var leftButton = document.getElementById("left-button");
+var buttonVoid = document.getElementById("button-void");
+
+//Filter
+var filterContent = document.getElementById("filter-content");
+var showFilter = document.getElementById("show");
+var arrow = document.getElementById("arrow");
+
+//thickbox's state
+var filterBool = true;
+var aboutBool = false;
+var personBool = false;
+
+
+//give all elements inside class: about, person....
+addClass("about");
+addClass("person");
+addClass("show-filter");
+
+//open or close thickboxes 
+document.addEventListener("click", function (e) {
+
+    if (filterBool == true && e.target.className == "show-filter") {
+        closeFilter();
+    }
+    else if (filterBool == false && e.target.className == "show-filter") {
+        openFilter();
+    }
+
+    else if (aboutBool == true && e.target.className != "about") {
+        closeAbout();
+    }
+    else if (aboutBool == false && e.target.id == "aboutButton") {
+        openAbout();
+    }
+
+    else if (personBool == true && (e.target.className != "person")) {
+        closePerson();
+    }
+    else if (personBool == false && e.target.className == "person-photo") {
+        openPerson();
+    }
+
+
+})
+
+function openAbout() {
+    aboutBool = true;
+    about.style.padding = "3em";
+    all.style.display = "block";
+    closeButton.style.display = "block";
+    disableScroll();
+
+    if (mq.matches) {
+        about.style.width = "70vw";
+    }
+    else if (!mq.matches) {
+        about.style.width = "90vw";
+    }
+
 }
+
+function closeAbout() {
+    aboutBool = false;
+    about.style.width = "0";
+    about.style.padding = "0";
+    all.style.display = "none";
+    allowScroll();
+}
+
+function openPerson() {
+    personBool = true;
+
+    person.style.padding = "2em";
+    all.style.display = "block";
+    rightButton.style.display = "block";
+    leftButton.style.display = "block";
+    disableScroll();
+
+    buttonVoid.style.display = "block";
+
+    if (mq.matches) {
+        person.style.width = "55vw";
+    }
+    else if (!mq.matches) {
+        person.style.width = "90vw";
+    }
+
+    buttonVoid.style.height = person.offsetHeight + "px";
+
+}
+
+function closePerson() {
+    personBool = false;
+    person.style.width = "0";
+    person.style.padding = "0";
+    all.style.display = "none";
+    rightButton.style.display = "none";
+    leftButton.style.display = "none";
+    buttonVoid.style.display = "none";
+    allowScroll()
+}
+
 function openFilter() {
     filterBool = true;
-    document.getElementById("filter-content").style.display = "flex";
-    document.getElementById("show").textContent = "HIDE FILTER";
-    document.getElementById("arrow").style.transform = 'rotate(360deg)';
+    filterContent.style.display = "flex";
+    showFilter.textContent = "HIDE FILTER";
+    arrow.style.transform = 'rotate(360deg)';
+}
+
+function closeFilter() {
+    filterBool = false;
+    filterContent.style.display = "none";
+    showFilter.textContent = "SHOW FILTER";
+    arrow.style.transform = 'rotate(180deg)';
 }
 
 function clearFilter() {
@@ -137,26 +152,26 @@ window.onresize = function () {
 
     //align buttons responsively
     if (personBool) {
-        document.getElementById("button-void").style.height = document.getElementById("person").offsetHeight + "px";
+        buttonVoid.style.height = person.offsetHeight + "px";
     }
 
     //if width is min-width:850px and #person is open
     if (mq.matches && personBool == true) {
-        document.getElementById("person").style.width = "55vw";
+        person.style.width = "55vw";
 
     }
     //else #person
     else if (personBool) {
-        document.getElementById("person").style.width = "90vw";
+        person.style.width = "90vw";
     }
 
     else if (mq.matches && aboutBool) {
-        document.getElementById("about").style.left = "20vw";
-        document.getElementById("about").style.width = "60vw";
+        about.style.left = "20vw";
+        about.style.width = "60vw";
     }
 
     else if (aboutBool) {
-        document.getElementById("about").style.left = "5vw";
-        document.getElementById("about").style.width = "90vw";
+        about.style.left = "5vw";
+        about.style.width = "90vw";
     }
 }
